2ea05d9adee98bcce489c94562c10be9370d5d15
ishant <ishantkohlikohli@gmail.com>
2026-06-17 19:11:25 +0530

    Bot added

---
+++ src/commands/Utility/modules/report_setchannel.js
@@ -0,0 +1,37 @@
+import { PermissionsBitField } from 'discord.js';
+import { errorEmbed, successEmbed } from '../../../utils/embeds.js';
+import { setLogChannel } from '../../../services/loggingService.js';
+import { InteractionHelper } from '../../../utils/interactionHelper.js';
+import { logger } from '../../../utils/logger.js';
+
+export default {
+    async execute(interaction, config, client) {
+        if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageGuild)) {
+            return InteractionHelper.safeReply(interaction, {
+                embeds: [errorEmbed('Permission Denied', 'You need **Manage Server** permissions to set the report channel.')],
+                ephemeral: true,
+            });
+        }
+
+        const channel = interaction.options.getChannel('channel');
+        const guildId = interaction.guildId;
+
+        try {
+            await setLogChannel(client, guildId, 'reports', channel.id);
+
+            return InteractionHelper.safeReply(interaction, {
+                embeds: [successEmbed(
+                    'Report Channel Set',
+                    `All new reports will now be sent to ${channel}.\nYou can also manage this from \`/logging dashboard\`.`,
+                )],
+                ephemeral: true,
+            });
+        } catch (error) {
+            logger.error('report_setchannel error:', error);
+            return InteractionHelper.safeReply(interaction, {
+                embeds: [errorEmbed('Database Error', 'Could not save the channel configuration.')],
+                ephemeral: true,
+            });
+        }
+    },
+};
